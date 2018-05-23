982701504bddc0f99d1d7d2c1c513c7ce5348526
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-23 12:11:18 +0800

    preliminary catalog page view responsive layout

--- pages/Catalog.vue
+++ pages/Catalog.vue
@@ -1,85 +1,79 @@
 <template>
-  <section id="catalog-view">
-    <div>top</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>catalog page</div>
-    <div>end</div>
+  <section id="catalog-view"
+           :class="classBinding">
+    <aside>
+      catalog menu
+    </aside>
+    <header>
+      breadcrumbs
+    </header>
+    <article>
+      product browser
+    </article>
+    <footer>
+      pagination
+    </footer>
   </section>
 </template>
 
 <script>
+import vuexMappers from 'vuex'
 export default {
   name: 'CatalogView',
   layout: 'default',
+  computed: {
+    ...vuexMappers.mapGetters('mobileDetect', {isMobile: 'isMobile'}),
+    classBinding() {
+      return {
+        'desktop-layout': !this.isMobile && this.$mq !== 'mobile',
+        'mobile-layout': this.isMobile || this.$mq === 'mobile',
+      }
+    },
+  },
 }
 </script>
+
+<style scoped>
+#catalog-view,
+aside,
+header,
+article,
+footer {
+  border: 1px solid blue;
+  box-sizing: border-box;
+}
+#catalog-view {
+  width: 100%;
+  height: 100%;
+  display: grid;
+}
+#catalog-view.desktop-layout {
+  grid-column-gap: 5px;
+  grid-row-gap: 5px;
+  grid-template-columns: max-content min-content auto;
+  grid-template-rows: min-content auto min-content;
+}
+#catalog-view.mobile-layout {
+  grid-template-columns: auto;
+  grid-template-rows: min-content auto min-content;
+}
+#catalog-view.desktop-layout aside {
+  grid-column: 1/2;
+  grid-row: 1/-1;
+}
+#catalog-view.mobile-layout aside {
+  display: none;
+}
+header {
+  grid-column: -2/-1;
+  grid-row: 1/2;
+}
+article {
+  grid-column: -2/-1;
+  grid-row: 2/3;
+}
+footer {
+  grid-column: -2/-1;
+  grid-row: 3/4;
+}
+</style>
