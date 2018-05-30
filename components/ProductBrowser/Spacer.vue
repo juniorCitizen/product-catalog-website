3907205d7bc805b34f5fa96ce5c28636aa41f2d3
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-31 00:03:45 +0800

    button pagination

--- components/ProductBrowser/Spacer.vue
+++ components/ProductBrowser/Spacer.vue
@@ -2,26 +2,32 @@
   <div class="spacer">
     <div class="field has-addons">
       <p class="control">
-        <a class="button is-small">
+        <a :disabled="isFirstPage"
+           class="button is-small"
+           @click="toPage(paginationInfo.currentPage-1)">
           <span class="icon is-small">
             <i class="fas fa-angle-left"/>
           </span>
-          <span>PREVIOUS</span>
+          <span>GO TO PREVIOUS PAGE</span>
+        </a>
+      </p>
+      <p v-if="isFirstPage"
+         class="control">
+        <a class="first-page button is-small">
+          <span>YOU ARE AT THE FIRST PAGE</span>
+        </a>
+      </p>
+      <p v-if="isLastPage"
+         class="control">
+        <a class="last-page button is-small">
+          <span>YOU ARE AT THE LAST PAGE</span>
         </a>
       </p>
       <p class="control">
-        <a class="button is-small">
-          <span>FIRST PAGE</span>
-        </a>
-      </p>
-      <p class="control">
-        <a class="button is-small">
-          <span>LAST PAGE</span>
-        </a>
-      </p>
-      <p class="control">
-        <a class="button is-small">
-          <span>NEXT</span>
+        <a :disabled="isLastPage"
+           class="button is-small"
+           @click="toPage(paginationInfo.currentPage+1)">
+          <span>GO TO NEXT PAGE</span>
           <span class="icon is-small">
             <i class="fas fa-angle-right"/>
           </span>
@@ -32,8 +38,34 @@
 </template>
 
 <script>
+import vuexMappers from 'vuex'
 export default {
   name: 'Spacer',
+  computed: {
+    ...vuexMappers.mapGetters('catalog', {
+      paginationInfo: 'paginationInfo',
+      breadcrumb: 'breadcrumb',
+    }),
+    isFirstPage() {
+      return this.paginationInfo.currentPage === 1
+    },
+    isLastPage() {
+      return this.paginationInfo.currentPage === this.paginationInfo.totalPages
+    },
+  },
+  methods: {
+    ...vuexMappers.mapActions('catalog', {
+      fetchProducts: 'fetchProducts',
+    }),
+    toPage(pageNumber) {
+      if (pageNumber !== this.currentPage) {
+        return this.fetchProducts({
+          category: this.activeCategory,
+          page: pageNumber,
+        })
+      }
+    },
+  },
 }
 </script>
 
@@ -47,5 +79,9 @@ export default {
 div.pagination-nav-button-container {
   margin-left: 15px;
   margin-right: 15px;
+}
+a.first-page,
+a.last-page {
+  pointer-events: none;
 }
 </style>
