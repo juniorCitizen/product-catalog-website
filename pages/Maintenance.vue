bbb7efa6f8dd7f5d3643ec470957ae727114d8a6
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-09-05 15:39:17 +0800

    carousel test code

--- pages/Maintenance.vue
+++ pages/Maintenance.vue
@@ -71,7 +71,7 @@ export default {
   left: 0;
   height: 100vh;
   width: 100vw;
-  background-color: rgba(0, 0, 0, 0.6);
+  background-color: rgba(0, 0, 0, 0.85);
 }
 .content-container {
   width: 100%;
